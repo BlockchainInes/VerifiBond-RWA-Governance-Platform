a9db1aeaf2c33db58c4883256e44acdd9d388f76
Ines Krueger <lernenmitines@yahoo.com>
2026-07-18 15:21:16 +0200

    Add deployment script for VerifiBond Token
    This script deploys the VerifiBond governance token to the specified network using the deployer's private key.

--- script/DeployVerifiBond.s.sol
+++ script/DeployVerifiBond.s.sol
@@ -1,1 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import {Script, console2} from "forge-std/Script.sol";
+import {VerifiBondToken} from "../src/VerifiBondToken.sol";
+
+/// @title Deploy VerifiBond Token
+/// @author Ines Krueger
+/// @notice Deploys the VerifiBond governance token to the selected network.
+contract DeployVerifiBond is Script {
+
+    function run() external {
+
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        VerifiBondToken token = new VerifiBondToken();
+
+        vm.stopBroadcast();
+
+        console2.log("VerifiBond deployed to:", address(token));
+    }
+}
